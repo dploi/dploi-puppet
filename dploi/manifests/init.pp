1495f92f2949f8380205d42ad4dd9524a2b3a19c
Kristian Øllegaard <kristian.ollegaard@divio.ch>
2011-11-07 16:28:16 +0100

    Updated the sudo file, since the old one was too restrictive. The new one, is however a bit too open, but at least it works for now.

--- dploi/manifests/init.pp
+++ dploi/manifests/init.pp
@@ -18,7 +18,7 @@ define dploi::customer($project, $state, $uid, $gid=502, $enabled="true", $loadb
         $tmpusername:
             user => "$tmpusername",
             privileges => [
-                "ALL=(root) NOPASSWD:/usr/bin/supervisorctl restart $tmpusername,/usr/bin/supervisorctl add $tmpusername,/usr/bin/supervisorctl update $tmpusername,/usr/bin/supervisorctl start $tmpusername,/usr/bin/supervisorctl stop $tmpusername,/usr/bin/supervisorctl status $tmpusername,/etc/init.d/nginx reload $tmpusername"
+                "ALL=(root) NOPASSWD:/usr/bin/supervisorctl restart $tmpusername*,/usr/bin/supervisorctl add $tmpusername*,/usr/bin/supervisorctl update $tmpusername*,/usr/bin/supervisorctl start $tmpusername*,/usr/bin/supervisorctl stop $tmpusername*,/usr/bin/supervisorctl status $tmpusername*,/etc/init.d/nginx reload"
             ]
     }
 }
