e618072ce9c0c0f1ad903d81319f06633b7ab621
Kristian Øllegaard <kristian.ollegaard@divio.ch>
2012-01-03 18:20:57 +0100

    Munin and Nagios monitoring

---
+++ munin/manifests/init.pp
@@ -0,0 +1,94 @@
+class munin {
+
+}
+
+class munin::client {
+	motd::register{"Munin client": }
+	package {
+		"munin-node":
+			ensure => latest;
+		"munin-plugins-extra":
+			ensure => latest;
+	}
+	service {
+		"munin-node":
+			ensure => running,
+			enable => true,
+			hasrestart => true,
+			hasstatus => true,
+	}
+    @@munin::register{"${::fqdn}": }
+	File <<| tag == "munin-node.conf" |>>
+    $pluginfile = "/etc/munin/plugin-conf.d/puppet_plugins.conf"
+
+    concat{$pluginfile:
+       owner => root,
+       group => root,
+       mode  => 644
+    }
+    concat::fragment{"munin_plugin_empty":
+       target  => $pluginfile,
+       content => ""
+    }
+}
+
+class munin::server {
+	motd::register{"Munin server": }
+	package { "munin":
+		ensure => latest;
+	}
+    include concat::setup
+    $concatfile = "/etc/munin/munin-conf.d/puppet.conf"
+
+    concat{$concatfile:
+       owner => root,
+       group => root,
+       mode  => 644
+    }
+
+    concat::fragment{"munin_header":
+       target => $concatfile,
+       content => "",
+       order   => 01,
+    }
+	Munin::Register <<||>>
+	@@file{
+		"/etc/munin/munin-node.conf":
+			content => template("munin/munin-node.conf.erb"),
+			tag => "munin-node.conf",
+			require => Package["munin-node"],
+	}
+}
+define munin::register($content="", $order=10) {
+   if $content == "" {
+      $body = $name
+   } else {
+      $body = $content
+   }
+
+   concat::fragment{"munin_node_$name":
+      target  => "/etc/munin/munin-conf.d/puppet.conf",
+      content => "[$body]\naddress $body\n"
+   }
+}
+
+define munin::plugin($pluginoptions="", $plugin="") {
+	if $plugin != "" {
+		$fname = $plugin
+	} else {
+		$fname = $name
+	}
+	file { "/etc/munin/plugins/$name":
+		ensure => link,
+		target => "/usr/share/munin/plugins/$fname",
+		notify => Service["munin-node"],
+		require => Package["munin-node"];
+	}
+	if $pluginoptions != ""{
+		concat::fragment{"munin_plugin_$name":
+			target  => "/etc/munin/plugin-conf.d/puppet_plugins.conf",
+			content => "[$name]\n${pluginoptions}"
+		}
+	}
+}
+
---
+++ nagios/manifests/init.pp
@@ -0,0 +1,82 @@
+class nagios
+{
+
+}
+class nagios::server {
+	motd::register{"Nagios server": }
+	# Put this in your own nagios class
+	# nagios_contact { "yourname":
+	# 	email => "your@email.com",
+	# 	service_notification_period => "24x7",
+	# 	host_notification_period => "24x7",
+	# 	service_notification_options => "w,u,c,r,f",
+	# 	host_notification_options => "d,r,f",
+	# 	service_notification_commands => "notify-service-by-email",
+	# 	host_notification_commands => "notify-host-by-email",
+	# 	target => "/etc/nagios3/conf.d/puppet.cfg";
+	# }
+	# nagios_contactgroup {"serveradmins":
+	# 	members => "yourname",
+	# 	target => "/etc/nagios3/conf.d/puppet.cfg";
+	# }
+	package {'nagios3':
+		alias => "nagios",
+		ensure => latest;
+	}
+	service {'nagios3':
+		alias => "nagios",
+		ensure => running,
+		enable => true,
+		hasrestart => true,
+		hasstatus => true,
+		require => Package['nagios3'],
+	}
+	# Symlink nagios, so it works with the puppet config
+	file { "/etc/nagios/":
+		ensure => link,
+		target => "/etc/nagios3/",
+		mode => 644,
+		require => Package["nagios3"];
+	}
+	# Collect everything else, which will be staying at the default level5…
+	Nagios_host <<||>>
+	Nagios_service <<||>>
+}
+
+class nagios::client {
+	motd::register{"Nagios monitored client (SSH+Ping)": }
+	# Nagios configuration
+	@@nagios_host { "${::fqdn}":
+		ensure => present,
+		alias => "${::hostname}",
+		address => "${::ipaddress}",
+		max_check_attempts =>  5,
+		use => "generic-host",
+		contact_groups => "serveradmins",
+		target => "/etc/nagios3/conf.d/puppet.cfg";
+	}
+	@@nagios_service { "${::hostname}_check_ping":
+		ensure => present,
+		host_name => "${::fqdn}",
+		notification_interval => 60,
+		flap_detection_enabled => 1,
+		service_description => "Ping",
+		check_command => "check_ping!300.0,20%!500.0,60%",
+		check_interval => "1",
+		contact_groups => "serveradmins",
+		use => "generic-service",
+		target => "/etc/nagios3/conf.d/puppet.cfg";
+	}
+	@@nagios_service { "${::hostname}_check_ssh":
+		ensure => present,
+		host_name => "${::fqdn}",
+		notification_interval => 60,
+		check_interval => "1",
+		flap_detection_enabled => 1,
+		service_description => "SSH",
+		check_command => "check_ssh",
+		contact_groups => "serveradmins",
+		use => "generic-service",
+		target => "/etc/nagios3/conf.d/puppet.cfg";
+	}
+}
