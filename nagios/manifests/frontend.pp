aa8b18e4fba439db1edefb3f3f81ca2b5089c0c9
Kristian Øllegaard <kristian.ollegaard@divio.ch>
2012-01-15 19:00:33 +0100

    nagios frontend

---
+++ nagios/manifests/frontend.pp
@@ -0,0 +1,25 @@
+class nagios::python-nagios-frontend {
+	# Alternative frontend for nagios
+	require supervisor
+	package {"python-nagios-frontend":
+		provider => "pip",
+		ensure => "latest",
+	}
+	
+	file {"/etc/python-nagios-frontend/":
+		ensure => "directory",
+		require => Package["python-nagios-frontend"],
+	}
+	
+	file {"/etc/python-nagios-frontend/config.xml":
+		content => template("nagios/python-nagios-frontend/config.xml.erb"),
+		require => File["/etc/python-nagios-frontend/"]
+	}
+	
+	supervisor::program{"python-nagios-frontend":
+		command => "python-nagios-frontend",
+		directory => "/etc/python-nagios-frontend/",
+		user => "www-data",
+		require => File["/etc/python-nagios-frontend/config.xml"]
+	}
+}
