fe5795648c31fb4d00bfb7964a816a1998e8c3aa
Kristian Øllegaard <kristian.ollegaard@divio.ch>
2012-01-15 19:00:58 +0100

    supervisor support for adding programs with puppet

---
+++ supervisor/manifests/program.pp
@@ -0,0 +1,9 @@
+define supervisor::program($command, $directory, $user, $autostart="True", $autorestart="True", $redirect_stderr="True", $environment="") {
+	require supervisor
+	file {"/etc/supervisor/conf.d/$name.conf":
+		content => template("supervisor/program.erb"),
+	}
+	exec {"/usr/bin/supervisorctl update $name":
+		
+	}
+}
